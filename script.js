782e7ed060fd7cb51fbc3c9d49cd997617cdd731
SoujanyaS <soujanyasuresh74@gmail.com>
2024-12-07 15:20:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example of a simple interactive element (if needed in the future)
+document.querySelector('button').addEventListener('click', function() {
+    alert('You clicked the button!');
+});
